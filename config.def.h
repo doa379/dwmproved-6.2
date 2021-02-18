d3bfe38f8a3174a34ef7449c3bced47042235cdc
doa379 <doa379@gmail.com>
2021-02-18 15:06:41 +0000

    ;

--- config.def.h
+++ config.def.h
@@ -114,6 +114,8 @@ static Button buttons[] = {
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
+	{ ClkTagBar,            0,              Button1,        focusstack,     {.i = +1 } },
+	{ ClkTagBar,            0,              Button3,        focusstack,     {.i = -1 } },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
